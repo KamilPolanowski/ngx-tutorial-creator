41e5fc07a72237e50d4d021b172c0c5eea3f1f8c
KamilPolanowski <kampolanowski44@gmail.com>
2019-01-16 11:45:38 +0100

    e tam nic takiego

--- ngx-tutorial-creator/src/tutorial-steps/tutorial-steps.component.ts
+++ ngx-tutorial-creator/src/tutorial-steps/tutorial-steps.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy, ViewEncapsulation, Input, Output, EventEmitter } from '@angular/core';
 import { StepperSelectionEvent } from '@angular/cdk/stepper';
 import { TutorialStep } from '../ngx-tc-interfaces';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-tc-tutorial-steps',
@@ -12,10 +13,33 @@ import { TutorialStep } from '../ngx-tc-interfaces';
 export class TutorialStepsComponent implements OnInit {
   @Input() public tutorialSteps: Array<TutorialStep>;
   @Output() public newSelectedStepsIndex: EventEmitter<number> = new EventEmitter;
+  public tutorialFormGroup: FormGroup;
+  private _fb: FormBuilder = new FormBuilder();
+
   constructor() { }
 
   ngOnInit() {
+    this.createFormGroups();
   }
+
+  createFormGroups(): void {
+    // this.tutorialFormGroup = this._fb.group(this.createFormControls());
+
+    // this creates an illusionary behaviour for stepper to highlight only one,
+    // active step
+    this.tutorialFormGroup = this._fb.group({
+      overallCtrl: ['', Validators.required]
+    });
+
+  }
+
+  /* createFormControls(): object {
+    const controls: object = {};
+    this.tutorialSteps.forEach((step, index) => {
+      controls[index] = new FormControl('', Validators.required);
+    });
+    return controls;
+  } */
 
   /**
    * Handling step change
